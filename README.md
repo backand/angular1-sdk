f168f81ae11b032ff459c96bc73aff90bd050595
Ran Cohen <rannn505@outlook.com>
2017-01-18 04:08:12 +0200

    readme

--- README.md
+++ README.md
@@ -1,9 +1,49 @@
-angular1bknd-sdk
+bkndangular1-sdk
 ===
+[![npm version](https://img.shields.io/npm/v/@backand/bkndangular1-sdk.svg?style=flat-square)](https://www.npmjs.org/package/@backand/bkndangular1-sdk)
+[![npm downloads](https://img.shields.io/npm/dt/@backand/bkndangular1-sdk.svg?style=flat-square)](http://npm-stat.com/charts.html?package=@backand/bkndangular1-sdk)
 
 >  Backand SDK for [Angularjs1](https://angularjs.org/).
 This SDK enables you to communicate comfortably and quickly with your Backand app.
-It wraps the [vanillabknd-sdk](https://github.com/backand/vanillabknd-sdk) to allow easier work on projects involving Angularjs1.
+It wraps the [bkndvanilla-sdk](https://github.com/backand/bkndvanilla-sdk) to allow easier work on projects involving Angularjs1.
+
+
+## Installation
+- npm:
+```bash
+$ npm i -S @backand/bkndangular1-sdk
+```
+- yarn:
+```bash
+$ yarn add @backand/bkndangular1-sdk
+```
+``` html
+<script src="node_modules/@backand/bkndangular1-sdk/backand.provider.min.js"></script>
+```
+- download/clone this repo and include `backand.provider.min.js` in your project
+``` html
+<script src="backand.provider.min.js"></script>
+```
+
+
+## Quick start
+```javascript
+angular
+  .module('myApp', ['backand'])
+  .config(function (BackandProvider) {
+    BackandProvider.init({
+      appName: 'APP_NAME',
+      anonymousToken: 'ANONYMOUS_TOKEN'
+    });
+  })
+  .controller('myAppCtrl', ['$scope', '$http', 'Backand', function myAppCtrl() {
+
+  }]);
+```
+
+
+## Examples
+***To view the demo web page, just run npm start - [example page](https://github.com/backand/bkndangular1-sdk/blob/master/example/).***
 
 
 ## License
--- example/realtime-todo/app.js
+++ example/realtime-todo/app.js
@@ -1,21 +1,17 @@
-var myApp = angular.module('myTodoApp', ['backand']);
-
-myApp.config(function (BackandProvider) {
-  BackandProvider.setAppName('backandtodoapp');
-  BackandProvider.setSignUpToken('76a0ed19-c9d4-405a-9e20-493d637b131c');
-  BackandProvider.setAnonymousToken('6adbc622-36b5-496c-b288-19ea28816f10');
-  BackandProvider.runSocket(true);
-})
-
-myApp.controller('DemoCtrl', ['$scope', '$http', 'Backand', DemoCtrl]);
+angular
+  .module('myTodoApp', ['backand'])
+  .config(function (BackandProvider) {
+    BackandProvider.setAppName('backandtodoapp');
+    BackandProvider.setSignUpToken('76a0ed19-c9d4-405a-9e20-493d637b131c');
+    BackandProvider.setAnonymousToken('6adbc622-36b5-496c-b288-19ea28816f10');
+    BackandProvider.runSocket(true);
+  })
+  .controller('DemoCtrl', ['$scope', '$http', 'Backand', DemoCtrl]);
 
 function DemoCtrl($scope, $http, Backand) {
-  var objectName = 'todo';
-
-  $scope.todos = [];
-  Backand.useAnonymousAuth();
-
   // console.log(Backand);
+  $scope.todos = [];
+  var objectName = 'todo';
 
   Backand.on('todo_updated', function (data) {
     //Get the event and refresh the list
@@ -31,20 +27,18 @@ function DemoCtrl($scope, $http, Backand) {
     }
     Backand.object.getList(objectName, params).then(function(response) {
       $scope.todos = response.data;
-    });
+    }).catch(err => console.log(err));
   };
   $scope.readList();
 
   $scope.create = function (newTodo) {
     return Backand.object.create(objectName, newTodo, {returnObject: true}).then(function(response) {
-      //$scope.readList();
       return response.data;
     });
   };
 
   $scope.update = function (todo) {
     return Backand.object.update(objectName, todo.id, todo, {returnObject: true}).then(function(response) {
-      //$scope.readList();
       return response.data;
     });
   };
