47c0e93a214b3d2c25a49168a7845ec371ef16e4
Itay Herskovits <itay@backand.com>
2017-07-02 12:30:51 +0300

    Release version 2.0.8

--- dist/backand.provider.js
+++ dist/backand.provider.js
@@ -1,6 +1,6 @@
 /*********************************************************
  * @backand/angular1-sdk - Backand SDK for Angular 1
- * @version v2.0.7
+ * @version v2.0.8
  * @link https://github.com/backand/angular1-sdk#readme
  * @copyright Copyright (c) 2017 Backand https://www.backand.com/
  * @license MIT (http://www.opensource.org/licenses/mit-license.php)
